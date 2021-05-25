11e90d90bd3760df3924ab1587d018bc6d658022
daekunhan <dosemfb0920@gmail.com>
2021-05-25 14:08:58 +0900

    material ui / join-room added

--- README.md
+++ README.md
@@ -6,3 +6,8 @@ House Party is a real-time p2p music player.
 Pull requests are welcome. For major changes, please open an issue first to discuss what you would like to change.
 
 Please make sure to update tests as appropriate.
+
+## Used Skills
+Python - Django
+React.js
+Material UI
--- music_controller/api/urls.py
+++ music_controller/api/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
-from .views import CreateRoomView, RoomView
+from .views import CreateRoomView, JoinRoom, RoomView, GetRoom
 urlpatterns = [
     path('room', RoomView.as_view()),
-    path('create-room', CreateRoomView.as_view())   
+    path('create-room', CreateRoomView.as_view()),   
+    path('get-room', GetRoom.as_view()),
+    path('join-room', JoinRoom.as_view())
 ]
